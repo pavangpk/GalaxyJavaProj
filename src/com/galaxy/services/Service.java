8d0c1654fad991bbc3e345c65550a3d92b8b358a
pavan <pavangpk@gmail.com>
2016-01-09 14:19:16 -0500

    Initial commit

---
+++ src/com/galaxy/services/Service.java
@@ -0,0 +1,11 @@
+package com.galaxy.services;
+
+public class Service {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Galaxy Java Proj");
+
+	}
+
+}
